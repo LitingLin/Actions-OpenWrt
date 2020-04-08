03b02d2d75d28afac39f9208536dfa915f01dd08
Liting Lin <linliting06@live.com>
2020-04-08 15:51:24 +0800

    update config

--- script/official/add-lean-packages.sh
+++ script/official/add-lean-packages.sh
@@ -1,4 +1,4 @@
-packages=( openwrt-fullconenat luci-app-zerotier luci-app-xlnetacc luci-app-mwan3helper luci-app-syncdial )
+packages=( openwrt-fullconenat luci-app-zerotier luci-app-xlnetacc luci-app-mwan3helper luci-app-syncdial luci-app-wrtbwmon luci-app-netdata luci-app-arpbind ddns-scripts_aliyun ddns-scripts_dnspod )
 
 git -C ../ clone --depth 1 https://github.com/coolsnowwolf/lede.git lean-lede
 mkdir package/lean
