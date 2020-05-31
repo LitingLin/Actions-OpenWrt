93cd725e1d081c2d74eb79869e3810f132d49367
Liting Lin <linliting06@live.com>
2020-06-01 04:09:00 +0800

    Add clash subconverter

---
+++ script/add-clash.sh
@@ -0,0 +1,13 @@
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+
+#pushd package/luci-app-openclash/luci-app-openclash/tools/po2lmo
+#make && make install
+#popd
+
---
+++ script/add-subconverter.sh
@@ -0,0 +1,1 @@
+git -C ./package/ clone --depth 1 https://github.com/tindy2013/openwrt-subconverter.git
