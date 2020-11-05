e50b608e318faf2a1611fb5b6656f362de8dc2f9
Liting Lin <linliting06@live.com>
2020-11-05 14:58:39 +0800

    add dnscrypt-proxy2

---
+++ script/add-luci-app-dnscrypt-proxy2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone --depth 1 https://github.com/peter-tank/luci-app-dnscrypt-proxy2.git ./package/luci-app-dnscrypt-proxy2
+
---
+++ script/add-minisign.sh
@@ -0,0 +1,2 @@
+git clone --depth 1 https://github.com/peter-tank/openwrt-minisign.git ./package/minisign
+
